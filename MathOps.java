b705d1d3b6071138740916c83edc51850e070790
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2026-05-02 12:08:35 +0530

    Add MathOps class to calculate sum of two numbers

---
+++ MathOps.java
@@ -0,0 +1,9 @@
+public class MathOps {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 25;
+        int sum = x + y;
+        
+        System.out.println("Sum of " + x + " + " + y + " = " + sum);
+    }
+}
