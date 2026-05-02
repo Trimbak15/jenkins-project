102034fa4e95d85a117d5e00f224c0550cbb26e3
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2026-05-02 12:08:12 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    // The main method is the entry point of any Java program
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints text to the console
+    }
+}
